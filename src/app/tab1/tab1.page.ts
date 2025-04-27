56095cfbead79624129140c8853d9e1eeecd9ab5
Flávio França <flavio.hmmf90@gmail.com>
2025-04-26 22:41:25 -0300

    Projeto ionic + angular, login e routeamento funcionando

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor(private router: Router) {}
+  usuario = '';
+  senha = '';
+  mensagemErro = '';
+  login() {
+    if (this.usuario === 'admin' && this.senha === 'admin') {
+      // console.log('Login bem-sucedido!');
+      this.router.navigate(['tabs/tab2']);
+    } else {
+      // this.mensagemErro = 'Usuário ou senha inválidos';
+      console.log('Login PAIA');
+    }
+  }
 }
+
+
+
+
