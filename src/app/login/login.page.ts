4b44c01925adee4418cb2830837ce2f407f3d982
Flávio França <flavio.hmmf90@gmail.com>
2025-05-15 22:33:44 -0300

    criacao da tela home

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -16,10 +16,14 @@ export class LoginPage implements OnInit {
   usuario : String = '';
   senha : String= '';
   mensagemErro : String ='';
+  mensagemSucesso : String ='';
   login() {
     if (this.usuario === 'admin' && this.senha === 'admin') {
       // console.log('Login bem-sucedido!');
-      
+      this.mensagemSucesso = 'Login bem-sucedido!';
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
       this.router.navigate(['./tabs/tab1']);
     } else { 
       this.mensagemErro = 'Usuário ou senha inválidos';
